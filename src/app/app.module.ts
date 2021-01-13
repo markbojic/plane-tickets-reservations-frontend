8444a993ba5a7e655f6315ba61c6e73ca9637984
Marko <marko321bojic@gmail.com>
2021-01-13 14:12:52 +0100

    navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common'
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+//import { NavbarModule } from './components/navbar/navbar.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 import { AuthService } from './services/auth/auth.service';
 import { AuthGuard } from '../app/auth.guard';
@@ -17,7 +19,8 @@ import { AuthGuard } from '../app/auth.guard';
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,37 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  navElement: HTMLElement = null;
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.navElement = <HTMLElement> document.getElementById("navbar");
+  }
+
+  @HostListener("window:scroll", ["$event"])
+  onScroll($event: Event) {
+    let scrollFactor = 200;
+    let opacity = (window.pageYOffset / scrollFactor);
+    opacity = opacity < 1 ? opacity : 1;
+
+    if (opacity <= 1) {
+      this.navElement.style.backgroundColor = "rgba(255, 255, 255, " + opacity + ")";
+    }
+
+    if (window.pageYOffset / scrollFactor > 1) {
+      this.navElement.classList.add("navbar-shadow");
+    } else {
+      this.navElement.classList.remove("navbar-shadow");
+    }
+  }
+
+}
